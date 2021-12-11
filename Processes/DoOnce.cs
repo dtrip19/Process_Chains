e1f7350263650aba61deb1ab86b8c254a0fbb369
dtrip19 <81382233+dtrip19@users.noreply.github.com>
2021-12-10 18:15:22 -0800

    Add files via upload

---
+++ Processes/DoOnce.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Processes
+{
+    class DoOnce : SubProcess
+    {
+        private bool hasBeenInvoked = false;
+        private Action action;
+
+        public override bool Invoke()
+        {
+            if (action != null && !action.Equals(null) && !hasBeenInvoked)
+                action.Invoke();
+
+            hasBeenInvoked = true;
+            return true;
+        }
+
+        public DoOnce(Action action)
+        {
+            this.action = action;
+        }
+    }
+}
---
+++ Processes/ProcessChain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Processes
+{
+    public class ProcessChain
+    {
+        public static event Action<ProcessChain> OnProcessComplete;
+
+        public List<SubProcess> subProcesses;
+        private bool hasLooped;
+
+        public ProcessChain()
+        {
+            subProcesses = new List<SubProcess>();
+            Program.OnUpdate += Loop;
+            OnProcessComplete += DestroyProcess;
+        }
+
+        private void Loop()
+        {
+            hasLooped = true;
+
+            for (int i = 0; i < subProcesses.Count; i++)
+            {
+                if (subProcesses[i] == null || subProcesses[i].Equals(null))
+                {
+                    subProcesses.RemoveAt(i);
+                    return;
+                }
+
+                if (!subProcesses[i].Invoke())
+                    return;
+
+                if (i == subProcesses.Count - 1)
+                    OnProcessComplete?.Invoke(this);
+            }
+        }
+
+        public void IterateRemotely() //Allows Iterate to be called remotely once separate from the Update loop to get things going immediately after initializing
+        {
+            if (!hasLooped)
+                Loop();
+        }
+
+        private static void DestroyProcess(ProcessChain process)
+        {
+            process = null;
+        }
+    }
+
+    public abstract class SubProcess
+    {
+        public abstract bool Invoke();
+    }
+}
